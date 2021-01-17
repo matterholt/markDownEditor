e87709f1d11985dd811d0031c915e3e7a598904c
MAtterholt <matterholt84@gmail.com>
2021-01-17 11:18:22 -0500

    refactor line update component

--- components/editor/HtmlConvertContainer.js
+++ components/editor/HtmlConvertContainer.js
@@ -1,33 +1,15 @@
 import {useState} from 'react'
 import { convertInput } from "../../utils/convertInput";
-import UpdateLineModal from "../docFeatures/UpdateLineModal";
 
-export default function HtmlConvertContainer({ mdUserLine, updateSelectInput }) {
+export default function HtmlConvertContainer({ mdUserLine, editPreviousLine }) {
   function createInnerHtml() {
     return {
       __html: convertInput(mdUserLine.lineContent),
     };
   }
-  
-  const [isEditable, setIsEditable] = useState(false);
-  const [editLine, setEditLine] = useState({});
-
-  function editPreviousLine(mdLine) {
-    setIsEditable(true);
-    setEditLine(mdLine);
-  }
-
 
   return (
     <>
-      {isEditable ? (
-        <UpdateLineModal
-          content={editLine}
-          setIsEditable={setIsEditable}
-          updateSelectInput={updateSelectInput}
-          setEditLine={setEditLine}
-        />
-      ) : null}
       <div
         key={mdUserLine.lineId}
         onClick={() => editPreviousLine(mdUserLine)}
--- components/editor/MkdownOut.js
+++ components/editor/MkdownOut.js
@@ -1,23 +1,39 @@
 import {useState} from 'react'
 
 import HtmlConvertContainer from "./HtmlConvertContainer";
-
+import UpdateLineModal from "../docFeatures/UpdateLineModal";
 
 const MkdownOut = ({ currentMarkdownRaw, updateSelectInput }) => {
 
+    const [isEditable, setIsEditable] = useState(false);
+    const [editLine, setEditLine] = useState({});
+
+    function editPreviousLine(mdLine) {
+      setIsEditable(true);
+      setEditLine(mdLine);
+    }
+
   return (
     <div className="inputContainer">
+      {isEditable ? (
+        <UpdateLineModal
+          content={editLine}
+          setIsEditable={setIsEditable}
+          updateSelectInput={updateSelectInput}
+          setEditLine={setEditLine}
+        />
+      ) : null}
       {currentMarkdownRaw.map((mdUserLine) => (
-          <HtmlConvertContainer
-            key={mdUserLine.lineId}
-            mdUserLine={mdUserLine}
-            updateSelectInput={updateSelectInput}
-          />
-        ))}
+        <HtmlConvertContainer
+          key={mdUserLine.lineId}
+          editPreviousLine={editPreviousLine}
+          mdUserLine={mdUserLine}
+        />
+      ))}
       <style jsx>{`
         .inputContainer {
           border-bottom: 1px solid white;
-          font-size:1em;
+          font-size: 1em;
         }
         pre {
           color: rgb(157, 174, 198);
