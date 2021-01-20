720c00a923179c3be1c563e508175f7c49c18731
MAtterholt <matterholt84@gmail.com>
2021-01-20 10:18:23 -0500

    working with test

--- __test__/editor.test.js
+++ __test__/editor.test.js
@@ -6,7 +6,7 @@ import Editor from '../pages/editor'
 describe("Functionality of editor page", () => {
     test("Renders the Editor page", () => {
         render(<Editor currentMarkdownHandlers />);
-        const header = getByText(/getting the Mark on/)
+        const header = screen.getByText(/getting/i)
         expect(header).toBeInTheDocument;
     });
 })
--- __test__/editorAction.test.js
+++ __test__/editorAction.test.js
@@ -1,27 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { renderHook } from "@testing-library/react-hooks";
+
+import { CurrentDocProvider } from "../context/DocPage-context";
 
 import EditorActions from "../components/editor/EditorActions";
 
+// add context
+
 describe("Functionality of editor actions", () => {
-  // test("Renders the Editor page", () => {
-  //     render(<EditorActions />);
-  //     screen.debug()
-  // });
-  //   test("Change title of Current Document", () => {
-  //     // add test
-  //   })
-    
-    test("Change font size of current Document", () => {
-      // add test
-      render(<EditorActions />)
-    })
-    
-//     test("Clears the mark down content", () => {
-//       // add test
-//     })
-//     test("Saves current markdown content to storage", () => {
-//         // add test
-//     })
-// });
+
+  test("Renders the Editor page", () => {
+
+    const { container } = render(<EditorActions currentMarkdownHandlers={currentMarkdownHandlers}/>);
+    console.log(container)
+
+  });
+
+})
