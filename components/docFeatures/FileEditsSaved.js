59b10630fd7a169151c8d4fe5e8bbcd1bc3c4198
MAtterholt <matterholt84@gmail.com>
2021-01-28 23:17:25 -0500

    working

--- components/docFeatures/FileEditsSaved.js
+++ components/docFeatures/FileEditsSaved.js
@@ -17,38 +17,10 @@ const SavedFiles = ({saveFileName, removedSelectedDoc}) => {
 
 
 function FileEditsSaved() {
-  const [state, setState] = useState();
+  const [state, setState] = useLocalStorage();
   const localKey = "savedFiles"
   
-  useEffect(() => {
-    // need to wait for the components to be mounted to the dom before accessing the window
-    const localStorageItems = window.localStorage.getItem("savedFiles");
- 
-    if (localStorageItems) {
-      console.log(localStorageItems);
-      // const localSavedItem = JSON.parse(localStorageItems);
 
-      // setState(localSavedItem);
-    } 
-    setState([
-      {
-        id: 1,
-        fileName: "test_2",
-        content: [
-          "## Doc Two test",
-          "another line for test",
-          "test test",
-          "last line of test",
-        ],
-      },
-    ]);
-  }, []);
-
-  useEffect(() => {
-    //  update local state when removed
-    const updateFilesForLocal = JSON.stringify(state);
-    window.localStorage.setItem(localKey, updateFilesForLocal);
-  }, [state]);
 
   function removedSelectedDoc(deletedFileName) {
     const updateList = state.filter((x) => x.fileName != deletedFileName);
--- components/docFeatures/SaveFile.js
+++ components/docFeatures/SaveFile.js
@@ -1,51 +1,48 @@
 import { useEffect } from "react";
-import { useCurrentDocState } from "../../context/DocPage-context";
-import useLocalStorage from "../../Hooks/useLocalStorage"
+import {useLocalStorage} from "../../Hooks/useLocalStorage"
 import { getRandomInt } from "../../utils/randomId";
 
 import {Button} from "../general/Button"
 
 function SaveFile({ currentMarkdownRaw, isDisabled, currentDocTitle }) {
-  // const { docState, UpdateDocState } = useCurrentDocState(); //one place for context
-  // const [docState, UpdateDocState] = useLocalStorage()
+//   const [savedFile, setSavedFile] = useLocalStorage()
+
+// useEffect(()=>{console.log(savedFile);})
+  function saveFile() {
+//     const currentlySavedFiles = savedFile;
+   
+//     if (currentMarkdownRaw.length === 0) {
+//       throw new Error("Document is empty");
+//     }
+//     const currentWorkingDoc = {
+//       id: getRandomInt(),
+//       fileName: currentDocTitle,
+//       content: currentMarkdownRaw,
+//     };
+
+//     // const savingFileCheck = currentlySavedFiles.find(
+//     //   (x) => x.fileName === currentDocTitle
+//     // );
+//       const savedListRemove = currentlySavedFiles.filter(
+//         (x) => x.fileName !== currentWorkingDoc.fileName
+//       );
+
+//       setSavedFile([...savedFile, currentWorkingDoc]);
+    }
   
 
 
-  function saveFile() {
-    const currentlySavedFiles = docState.savedFiles;
-    console.log(currentlySavedFiles);
-    if (currentMarkdownRaw.length === 0) {
-      throw new Error("Document is empty");
-    }
 
-    const savingFileCheck = currentlySavedFiles.find(
-      (x) => x.fileName === currentDocTitle
-    );
 
-    // need a better way to calculate the id
-    const currentDoc = {
-      id: getRandomInt(),
-      fileName: currentDocTitle,
-      content: currentMarkdownRaw,
-    };
 
-    const savedListRemove = currentlySavedFiles.filter(
-      (x) => x.fileName !== currentDoc.fileName
-    );
 
-    if (savingFileCheck) {
-      let updatedSaveFiles = [...savedListRemove, currentDoc];
-      UpdateDocState({
-        name: "savedFiles",
-        value: updatedSaveFiles,
-      });
-    } else {
-      UpdateDocState({
-        name: "savedFiles",
-        value: [...currentlySavedFiles, currentDoc],
-      });
-    }
-  }
+
+
+
+
+
+
+
 
   return (
     <div>
